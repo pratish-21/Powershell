726c7ae72f8fdb4ad3559b792b2ec152d7cd7018
pratish-21 <jangampratish1@gmail.com>
2025-06-22 16:31:30 +0530

    operators

---
+++ operators.ps1
@@ -0,0 +1,26 @@
+#arthimetic operators + - / * %
+1+2
+3-2
+3/5
+4*6
+5%7
+
+#assignment operator =, -=, *=, /=, %=
+$a = 4
+$b=2
+
+$a=$b
+$a-=$b
+$a+=$b
+$a*=$b
+$a/=$b
+$a%=$b
+
+#logical operator
+$a=40
+$b =49
+$a -and $b
+$a -or $b
+$a -xor $b
+#$a -not $b
+
--- output.ps1
+++ output.ps1
@@ -1,4 +1,4 @@
-$input = Read-Host "enter your name"
+<#$input = Read-Host "enter your name"
 Write-Host $input -ForegroundColor DarkCyan -BackgroundColor DarkMagenta
 Get-ChildItem | Format-Table 
 
@@ -21,26 +21,52 @@ Get-Process | Where-Object {$_.CPU -gt 10}
 
 $lines = Get-Content "C:\example.txt"
 foreach ($line in $lines) {
-    Write-Host $line
+Write-Host $line
 }
 
 
-    # Read from file
-    Get-Content < "input.txt"
+# Read from file
+Get-Content < "input.txt"
 
-    # Write to file
-    "Some text" > "output.txt"
+# Write to file
+"Some text" > "output.txt"
 
-    # Append to file
-    "More text" >> "output.txt"
+# Append to file
+"More text" >> "output.txt"
 
 
-        Write-Host "This is a message."
+Write-Host "This is a message."
 
-            Write-Output "This is output." | Get-Member
+Write-Output "This is output." | Get-Member
 
-                Get-Process | Out-File -FilePath "C:\processes.txt"
+Get-Process | Out-File -FilePath "C:\processes.txt"
 
-                    Get-Process > "processes.txt"
+Get-Process > "processes.txt"
 
-                        Get-Process | Tee-Object -FilePath "C:\processes.txt"
+Get-Process | Tee-Object -FilePath "C:\processes.txt"
+#>
+###################################################################3
+$name = Read-Host "enter your name"
+Write-Host $name -ForegroundColor DarkBlue -BackgroundColor Blue
+
+$filedetails = Get-Content "C:\Users\pratish\Desktop\cities.txt" #Get-Content "path of the file"
+write-host "File contents : $filedetails" -ForegroundColor DarkBlue -BackgroundColor yellow
+
+Get-service | Where-Object {$_.DisplayName -eq "Windows Update"} 
+
+Write-Host "This is a message" -ForegroundColor green
+Write-Output "this is for pipeline use"
+
+Get-Process | Out-File C:\Users\pratish\Desktop\process1.csv
+
+####################################################3
+
+$webresult = Invoke-WebRequest -uri "https://reddit.com/r/powershell.json"
+$raw = $webresult.content
+$data = $raw | ConvertFrom-Json
+$posts = $data.data.children.data
+$posts | Select-Object Title , Score | Sort-Object score -Descending
+
+######################################
+
+Get-ChildItem | out-gridview
